5936f5c2af0e668b8815ea752b0ea172aa129247
maciej-pajak <maciek.pajak@vp.pl>
2018-04-17 20:55:33 +0200

    create helper methods with tests

---
+++ src/main/java/pl/maciejpajak/classifier/Nd4jHelper.java
@@ -0,0 +1,27 @@
+package pl.maciejpajak.classifier;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.SpecifiedIndex;
+
+public final class Nd4jHelper {
+
+    private Nd4jHelper() {}
+
+    public static INDArray getSpecifiedElements(INDArray source, INDArray columnIndices) {
+        int rowsCount = source.size(0);
+        INDArray indices = Nd4j.linspace(0, rowsCount - 1, rowsCount)
+                .muli(source.size(1))
+                .addi(columnIndices);
+        return Nd4j.toFlattened(source).get(new SpecifiedIndex(indices.data().asInt())).transpose();
+    }
+
+    public static INDArray getSpecifiedElementsLoop(INDArray source, INDArray columnIndices) {
+        int rowsCount = source.size(0);
+        INDArray result = Nd4j.create(rowsCount, 1);
+        for (int i = 0 ; i < rowsCount ; i++) {
+            result.put(i, 0, source.getDouble(i, columnIndices.getInt(0, i)));
+        }
+        return result;
+    }
+}
---
+++ src/test/java/pl/maciejpajak/classifier/Nd4jHelperTest.java
@@ -0,0 +1,29 @@
+package pl.maciejpajak.classifier;
+
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import static org.junit.Assert.*;
+
+public class Nd4jHelperTest {
+
+    @Test
+    public void getSpecifiedElements() {
+        INDArray array = Nd4j.linspace(0, 11, 12).reshape(3,4);
+        INDArray columnIndexes = Nd4j.create(new double[] {2, 3, 1}, new int[] {1, 3});
+        // 0 1 2 3
+        // 4 5 6 7
+        // 8 9 10 11
+        assertEquals(Nd4j.create(new double[] {2, 7, 9}, new int[]{3, 1}), Nd4jHelper.getSpecifiedElements(array, columnIndexes));
+    }
+
+
+    @Test
+    public void getSpecifiedElementsLoop() {
+        INDArray array = Nd4j.linspace(0, 11, 12).reshape(3,4);
+        INDArray columnIndexes = Nd4j.create(new double[] {2, 3, 1}, new int[] {1, 3});
+        assertEquals(Nd4j.create(new double[] {2, 7, 9}, new int[]{3, 1}), Nd4jHelper.getSpecifiedElementsLoop(array, columnIndexes));
+    }
+
+}

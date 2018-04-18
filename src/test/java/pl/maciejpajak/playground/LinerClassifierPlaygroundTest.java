1550f364619f73967cc1cf35667ddea0a299f434
maciej-pajak <maciek.pajak@vp.pl>
2018-04-18 23:05:28 +0200

    add test

--- src/test/java/pl/maciejpajak/playground/LinerClassifierPlaygroundTest.java
+++ src/test/java/pl/maciejpajak/playground/LinerClassifierPlaygroundTest.java
@@ -1,8 +1,10 @@
 package pl.maciejpajak.playground;
 
+import javafx.util.Pair;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import pl.maciejpajak.classifier.LinearClassifier;
 
 import static org.junit.Assert.*;
 
@@ -14,14 +16,19 @@ public class LinerClassifierPlaygroundTest {
                 0.2, 0.8 ,1.4, 2.0, 2.6,
                 0.3, 0.9, 1.5, 2.1, 2.7,
                 0.4, 1.0, 1.6, 2.2, 2.8}, new int[]{4, 5});
-        INDArray y = Nd4j.create(new double[]{0, 1, 2, 3, 4}, new int[]{1, 5});
+        INDArray y = Nd4j.create(new double[]{0, 1, 2, 3, 4}, new int[]{5, 1});
         INDArray W = Nd4j.create(new double[]{0.1, 0.6, 1.1, 1.6,
                 0.2, 0.7, 1.2, 1.7,
                 0.3, 0.8, 1.3, 1.8,
                 0.4, 0.9, 1.4, 1.9,
                 0.5, 1.0, 1.5, 2.0}, new int[]{5, 4});
+        LinearClassifier.LossFunction.SVM.loss(x.transpose(),y,W.transpose(), 0);
+//        assertEquals(16.86, LinerClassifierPlayground.L(W, y, x.transpose()), 0.01);
+        Pair<Double, INDArray> resNaive = LinearClassifier.LossFunction.SVM_NAIVE.loss(x.transpose(),y,W.transpose(),0);
 
-        assertEquals(16.86, LinerClassifierPlayground.L(W, y, x), 0.01);
-
+        Pair<Double, INDArray> resVec = LinearClassifier.LossFunction.SVM.loss(x.transpose(),y,W.transpose(),0);
+        assertEquals(16.86 / 5, resNaive.getKey(), 0.01);
+        assertEquals(16.86 / 5, resVec.getKey(), 0.01);
+        assertEquals(resNaive.getValue(), resVec.getValue());
     }
 }

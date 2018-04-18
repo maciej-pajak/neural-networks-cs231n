8e15e48bef301f20668990a0b6e58c3753fbd28e
maciej-pajak <maciek.pajak@vp.pl>
2018-04-18 23:06:18 +0200

    fix helper

--- src/main/java/pl/maciejpajak/classifier/Nd4jHelper.java
+++ src/main/java/pl/maciejpajak/classifier/Nd4jHelper.java
@@ -4,6 +4,7 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.SpecifiedIndex;
 
+
 public final class Nd4jHelper {
 
     private Nd4jHelper() {}
@@ -12,7 +13,7 @@ public final class Nd4jHelper {
         int rowsCount = source.size(0);
         INDArray indices = Nd4j.linspace(0, rowsCount - 1, rowsCount)
                 .muli(source.size(1))
-                .addi(columnIndices);
+                .add(columnIndices.transpose());
         return Nd4j.toFlattened(source).get(new SpecifiedIndex(indices.data().asInt()));
     }
 
@@ -28,14 +29,14 @@ public final class Nd4jHelper {
     public static void putValues(INDArray destination, INDArray columnIndices, INDArray values) {
         int rowsCount = destination.size(0);
         for (int i = 0 ; i < rowsCount ; i++) {
-            destination.putScalar(i, columnIndices.getInt(0, i), values.getDouble(i, 0));
+            destination.putScalar(i, columnIndices.getInt(i, 0), values.getDouble(i, 0));
         }
     }
 
     public static void putScalar(INDArray destination, INDArray columnIndices, double value) {
         int rowsCount = destination.size(0);
         for (int i = 0 ; i < rowsCount ; i++) {
-            destination.putScalar(i, columnIndices.getInt(0, i), value);
+            destination.putScalar(i, columnIndices.getInt(i, 0), value);
         }
     }
 }

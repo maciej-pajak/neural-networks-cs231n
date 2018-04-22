855b2bffc59c6491345ec4e2fca3e40bb2d698ef
maciej-pajak <maciek.pajak@vp.pl>
2018-04-22 12:19:40 +0200

    add analyzing and plotting

--- src/main/java/pl/maciejpajak/casestudy/TwoLayerNetworkCaseStudy.java
+++ src/main/java/pl/maciejpajak/casestudy/TwoLayerNetworkCaseStudy.java
@@ -1,5 +1,8 @@
 package pl.maciejpajak.casestudy;
 
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.ops.transforms.Transforms;
@@ -33,6 +36,8 @@ public class TwoLayerNetworkCaseStudy {
     // second layer biases 1 x C
     private final INDArray biasesTwo;
 
+    private INDArray learningHistory;
+
     /**
      * Initialize the model. Weights are initialized to small random values and
      * biases are initialized to zero.
@@ -54,30 +59,26 @@ public class TwoLayerNetworkCaseStudy {
      *
      * @param trainingData - an array of shape N x D giving training data.
      * @param trainingLables - an array of shape N x 1 giving training labels.
-     * @param validationData - an array of shape N_VAL x D giving validation data.
-     * @param validationLables - an array of shape N_VAL x D giving validation labels.
      * @param learningRate - rate of optimization.
-     * @param learningRateDecay - factor used to decay the learning rate after each epoch.
      * @param reg - regularization strength.
      * @param iterations - number of iterations.
-     * @param batchSize - number of training examples to use per step.
      */
-    public void train(INDArray trainingData, INDArray trainingLables, INDArray validationData, INDArray validationLables,
-                      double learningRate, double learningRateDecay, double reg, double iterations, double batchSize) {
+    public void train(INDArray trainingData, INDArray trainingLables, double learningRate, double reg, int iterations) {
 
         int samples = trainingData.size(0);
+        learningHistory = Nd4j.create(iterations / 100, 3);
 
-        for (int i = 0 ; i < iterations ; i++) {
+        for (int i = 1 ; i <= iterations ; i++) {
             // evaluate class scores N x C
-            INDArray hiddenLayer = Transforms.max(trainingData.mmul(weightsOne).add(biasesOne), 0); // ReLU activation
-            INDArray scores = hiddenLayer.mul(weightsTwo).add(biasesTwo);
+            INDArray hiddenLayer = Transforms.max(trainingData.mmul(weightsOne).addRowVector(biasesOne), 0); // ReLU activation
+            INDArray scores = hiddenLayer.mmul(weightsTwo).addRowVector(biasesTwo);
 
             // compute the class probabilities
             INDArray expScores = Transforms.exp(scores);
             INDArray probs = expScores.divColumnVector(expScores.sum(1));
 
-            // compute hte loss: average cross-entropy loss and regularization
-            INDArray correctLogProbs = Transforms.log(probs).mul(-1);
+            // compute the loss: average cross-entropy loss and regularization
+            INDArray correctLogProbs = Transforms.log(Nd4jHelper.getSpecifiedElements(probs, trainingLables)).mul(-1);
 
             double dataLoss = correctLogProbs.sumNumber().doubleValue() / samples;
             double regLoss = Transforms.pow(weightsOne, 2).sumNumber().doubleValue() * reg
@@ -85,7 +86,10 @@ public class TwoLayerNetworkCaseStudy {
             double loss = dataLoss + regLoss;
 
             if (i % 100 == 0) {
-                LOG.info(String.format("iteration %d, loss %f", i, loss));
+                INDArray predicted = this.predict(trainingData);
+                double acc = predicted.eq(trainingLables).sumNumber().doubleValue() / trainingLables.length();
+                learningHistory.putRow(i / 100 - 1, Nd4j.create(new double[]{i, acc, loss}, new int[]{1,3}));
+                LOG.info(String.format("iteration %d / %d, acc %f, loss %f", i, iterations, acc, loss));
             }
 
             // compute the gradient on scores
@@ -102,21 +106,21 @@ public class TwoLayerNetworkCaseStudy {
             INDArray dHiddenLayer = dScores.mmul(weightsTwo.transpose());
 
             // backprop the ReLU non-linearity
-            dHiddenLayer.mul(hiddenLayer.gt(0)); // TODO check dhidden[hidden_layer <= 0] = 0
+            dHiddenLayer.muli(hiddenLayer.gt(0)); // TODO check dhidden[hidden_layer <= 0] = 0
 
             // finally into W,b
             INDArray dWeightsOne = trainingData.transpose().mmul(dHiddenLayer);
             INDArray dBiasesOne = dHiddenLayer.sum(0);
 
             // add regularization gradient contribution
-            dWeightsTwo.add(weightsTwo.mul(2.0 * reg));
-            dWeightsOne.add(weightsOne.mul(2.0 * reg));
+            dWeightsTwo.addi(weightsTwo.mul(2.0 * reg));
+            dWeightsOne.addi(weightsOne.mul(2.0 * reg));
 
             // perform a parameter update
-            weightsOne.add(dWeightsOne.mul(-1.0 * learningRate));
-            biasesOne.add(dBiasesOne.mul(-1.0 * learningRate));
-            weightsTwo.add(dWeightsTwo.mul(-1.0 * learningRate));
-            biasesTwo.add(dBiasesTwo.mul(-1.0 * learningRate));
+            weightsOne.addi(dWeightsOne.mul(-1.0 * learningRate));
+            biasesOne.addi(dBiasesOne.mul(-1.0 * learningRate));
+            weightsTwo.addi(dWeightsTwo.mul(-1.0 * learningRate));
+            biasesTwo.addi(dBiasesTwo.mul(-1.0 * learningRate));
         }
 
 
@@ -130,10 +134,61 @@ public class TwoLayerNetworkCaseStudy {
      */
     public INDArray predict(INDArray dataSet) {
 
-        INDArray hiddenLayer = dataSet.mmul(weightsOne).add(biasesOne);
-        INDArray scores = hiddenLayer.mmul(weightsTwo).add(biasesTwo);
+        INDArray hiddenLayer = Transforms.max(dataSet.mmul(weightsOne).addRowVector(biasesOne), 0);
+        INDArray scores = hiddenLayer.mmul(weightsTwo).addRowVector(biasesTwo);
 
         return scores.argMax(1);
+    }
+
+    public void printLearningAnalysis() {
+        // Create Chart
+        XYChart chart = new XYChartBuilder().width(600).height(500).title("Learning analysis").xAxisTitle("iteration").yAxisTitle("val").build();
+
+        double[] xIterations = learningHistory.getColumn(0).dup().data().asDouble();
+        double[] yAccuracy = learningHistory.getColumn(1).dup().data().asDouble();
+        double[] yLoss = learningHistory.getColumn(2).div(learningHistory.getColumn(2).maxNumber()).dup().data().asDouble(); // rescale loss to percentage
+
+        // Series
+        chart.addSeries("Accuracy [%]", xIterations, yAccuracy);
+        chart.addSeries("Loss [%  of max loss]", xIterations, yLoss);
+
+        new SwingWrapper(chart).displayChart();
+    }
+
+    public static void main(String[] args) {
+        // generate data that is not lineary separable
+        final int pointsPreClass = 100;
+        final int dimensionality = 2;
+        final int numberOfClasses = 3;
+
+        INDArray dataSet = Nd4j.zeros(pointsPreClass * numberOfClasses, dimensionality);
+        INDArray dataLabels = Nd4j.zeros(pointsPreClass * numberOfClasses, 1);
+
+        for (int i = 0 ; i < numberOfClasses ; i++) {
+            // INDArray ix = Nd4j.linspace(pointsPreClass * i, pointsPreClass * (i + 1) - 1, pointsPreClass);
+            INDArray radius = Nd4j.linspace(0.0, 1,pointsPreClass);
+            INDArray theta = Nd4j
+                    .linspace(i * 4, (i + 1) * 4, pointsPreClass)
+                    .addi(Nd4j.randn(1, pointsPreClass).muli(0.2));
+            INDArray coord = Nd4j.vstack(radius.mul(Transforms.sin(theta)), radius.mul(Transforms.cos(theta))).transpose();
+
+            for (int j = 0 ; j < pointsPreClass ; j++) { // TODO refactor to utilize Nd4j without loop
+                dataSet.putRow(i * pointsPreClass + j, coord.getRow(j));
+                dataLabels.putScalar(i * pointsPreClass + j, i);
+            }
+        }
+
+        TwoLayerNetworkCaseStudy network =
+                new TwoLayerNetworkCaseStudy(dataSet.size(1), 100, 3, 0.01);
+
+        network.train(dataSet, dataLabels,0.5, 0.001,10000);
+
+        INDArray predicted = network.predict(dataSet);
+        double acc = predicted.eq(dataLabels).sumNumber().doubleValue() / dataLabels.length();
+
+        LOG.info(String.format("final accuracy = %.2f", acc));
+
+        network.printLearningAnalysis();
     }
 
 }

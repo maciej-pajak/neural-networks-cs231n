f7eeabd87dd235b7b86c500c340a5742a9898f51
mpajak <mpajak@amelco.co.uk>
2018-04-27 17:34:54 +0100

    add tests

--- src/test/java/pl/maciejpajak/util/Nd4jHelperTest.java
+++ src/test/java/pl/maciejpajak/util/Nd4jHelperTest.java
@@ -1,4 +1,4 @@
-package pl.maciejpajak.classifier;
+package pl.maciejpajak.util;
 
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -12,7 +12,7 @@ public class Nd4jHelperTest {
     @Test
     public void getSpecifiedElements() {
         INDArray array = Nd4j.linspace(0, 11, 12).reshape(3,4);
-        INDArray columnIndexes = Nd4j.create(new double[] {2, 3, 1}, new int[] {1, 3});
+        INDArray columnIndexes = Nd4j.create(new double[] {2, 3, 1}, new int[] {3, 1});
         // 0 1 2 3
         // 4 5 6 7
         // 8 9 10 11
@@ -41,5 +41,29 @@ public class Nd4jHelperTest {
 
         assertEquals(expected, array);
         assertArrayEquals(expected.shape(), array.shape());
+    }
+
+    @Test
+    public void putValuesTest() {
+        INDArray array = Nd4j.linspace(0, 11, 12).reshape(3,4);
+        INDArray columnIndexes = Nd4j.create(new double[] {2, 3, 1}, new int[] {3, 1});
+        INDArray valuesToPut = Nd4j.create(new double[] {14, 15, 16}, new int[] {3, 1});
+
+        INDArray expected = Nd4j.create(new double[] {0, 1, 14, 3, 4, 5, 6, 15, 8, 16, 10, 11}, new int[] {3, 4});
+        Nd4jHelper.putValues(array, columnIndexes, valuesToPut);
+
+        assertTrue(expected.equals(array));
+    }
+
+    @Test
+    public void putScalarTest() {
+        INDArray array = Nd4j.linspace(0, 11, 12).reshape(3,4);
+        INDArray columnIndexes = Nd4j.create(new double[] {2, 3, 1}, new int[] {3, 1});
+        INDArray valuesToPut = Nd4j.ones(3,1);
+
+        INDArray expected = Nd4j.create(new double[] {0, 1, 1, 3, 4, 5, 6, 1, 8, 1, 10, 11}, new int[] {3, 4});
+        Nd4jHelper.putScalar(array, columnIndexes, 1);
+
+        assertTrue(expected.equals(array));
     }
 }
